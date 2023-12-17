29efc3174ffee3ecca716bf17bbecde98bbde854
Nalan <148653969+Nalan1413@users.noreply.github.com>
2023-12-18 00:01:32 +0100

    Update backup_image.py

--- Tensorflow/Softmax_&_Fashion_MNIST/backup_image.py
+++ Tensorflow/Softmax_&_Fashion_MNIST/backup_image.py
@@ -5,19 +5,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-class ImageDone:
-    def __init__(self, path):
-        self.original_image = Image.open(path)
-        grayscale_image = self.original_image.convert("L")
-        self.resized_image = grayscale_image.resize((28, 28))
-        self.image_array = np.array(self.resized_image)
-
-    # Getting the gray image batch
-    def grayscale_image_array(self):
-        image_batch = np.expand_dims(self.image_array, axis=0)
-        return image_batch
-
-    # Showing the image in 28 * 28 formation
-    def show_image(self):
-        plt.imshow(self.image_array)
-        plt.show()
+def ImageDone(path):
+    original_image = Image.open(path)
+    grayscale_image = original_image.convert("L")
+    inverted_image = Image.eval(grayscale_image, lambda x: 255 - x)
+    resized_image = inverted_image.resize((28, 28))
+    image_array = np.array(resized_image)
+    image_batch = np.expand_dims(image_array, axis=0)
+    return image_batch
+    
