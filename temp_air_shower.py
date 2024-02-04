6e3b84d50af4d0a5dd13f1a20e4bef5b45709d2c
Nalan <148653969+Nalan1413@users.noreply.github.com>
2024-02-04 13:13:05 +0100

    Update temp_air_shower.py

--- temp_air_shower.py
+++ temp_air_shower.py
@@ -6,65 +6,75 @@ from mpl_toolkits.mplot3d import Axes3D
 # Proton
 pythia = pythia8.Pythia()
 pythia.readString("Beams:idA = 2212")
-pythia.readString("Beams:eA = 10000")
+pythia.readString("Beams:eA = 2600")
 
 # Nitrogen - 14
 pythia.readString("Beams:idB = 1000070140")
 pythia.readString("Beams:eB = 14")
-pythia.readString("Beams:frameType = 2")
 
-# I am not sure what this part is supposed to do
-pythia.readString("HeavyIon:SigFitErr = "
-                  "0.02,0.02,0.1,0.05,0.05,0.0,0.1,0.0")
-pythia.readString("HeavyIon:SigFitNGen = 20")
-pythia.readString("HeavyIon:SigFitDefPar = 26.06,1.98,0.50")
-
-# Set up
+pythia.readString("Beams:frameType = 3")
+pythia.readString("HeavyIon:SigFitNGen = 0")
+pythia.readString("HeavyIon:SigFitDefPar = 29.95,2.19,0.60")
 pythia.readString("HardQCD:all = on")
+pythia.readString("PartonLevel:MPI = on")
+pythia.readString("MultipartonInteractions:alphaSvalue = 0.130")
+pythia.readString("MultipartonInteractions:pTmaxMatch = 0")
+pythia.readString("MultipartonInteractions:bProfile = 1")
+pythia.readString("MultipartonInteractions:allowRescatter = on")
+
 pythia.init()
-pythia.next()
 event = pythia.event
 
-# Data
+for events in range(1, 51):
+    pythia.next()
+    print(f"Number of particles {event.size()}")
+    if events % 5 == 0:
+        collision_count = 0
+        for particle in event:
+
+            # Debug
+            if particle.isFinal() != 10:
+                if particle.id() == 1000070140:
+                    collision_count += 1
+                    print(f"event {events} number of N14: {collision_count}")
+
+            # Add particles and check for collision
+            if particle.id() == 2212 and particle.isFinal():
+                print("found")
+                # Particle::Particle(int id, int status = 0, int mother1 = 0, int mother2 = 0, int daughter1 = 0, int daughter2 = 0,
+                # int col = 0, int acol = 0, double px = 0., double py = 0., double pz = 0., double e = 0., double m = 0., double scale = 0., double pol = 9.)
+                n14 = pythia8.Particle(1000070140, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 14, 13144.859)
+                n14.vProd(particle.xProd(), particle.yProd(), particle.zProd(), particle.tProd())
+                event.append(n14)
+                print(n14.xProd(),n14.yProd(),n14.zProd())
+
+# Init data and Graphs
+shower_particles = [particle for particle in event]
+particle_x = [particle.xProd() for particle in shower_particles]
+particle_y = [particle.yProd() for particle in shower_particles]
+particle_z = [particle.zProd() for particle in shower_particles]
+particle_en = [particle.e() for particle in shower_particles]
+
 energies = []
 par_dict = {}
 for i in range(event.size()):
-
     particle = event[i]
     particle_name = event[i].name()
 
-    if particle.isFinal() and particle.isParton() and pythia8.pythia.mayDecay(particle.id()):
-        pythia8.pythia.forceTimeDecay(particle.id())
-
-    elif particle.isFinal():
+    if particle.isFinal():
         energies.append(particle.e())
 
-    elif particle_name in par_dict:
+    if particle_name in par_dict:
         par_dict[particle_name] += 1
     else:
         par_dict[particle_name] = 1
 
-# print(f"    Particle {i + 1}: ID = {particle.id()}, PT = {particle.pT()}, Eta = {particle.eta()}, Phi = {particle.phi()}, Mass = {particle.m()}, Charge = {particle.charge()}")
-
-# final_state_particles = [prt for prt in pythia.event if prt.status() > 0]
-shower_particles = [particle for particle in event]
 
 parent_ids_m1 = [particle.mother1() for particle in shower_particles]
 parent_ids_m2 = [particle.mother2() for particle in shower_particles]
 particle_eta = [particle.eta() for particle in shower_particles]
 particle_phi = [particle.phi() for particle in shower_particles]
-particle_en = [particle.e() for particle in shower_particles]
-particle_x = [particle.xProd() for particle in shower_particles]
-particle_y = [particle.yProd() for particle in shower_particles]
-particle_z = [particle.zProd() for particle in shower_particles]
 
-print("energy of the 1st collision product:")
-p = pythia.event[3]
-print(f"{p.e():.2f} GeV")
-print("Identity of the 1st collision product:")
-print(p.id())
-print("Energies:", energies)
-print(par_dict)
 pythia.stat()
 
 plt.figure(figsize=(10, 10))
@@ -81,25 +91,15 @@ plt.xlabel('Particle Name')
 plt.ylabel('Frequency')
 plt.title('Particle ID Distribution')
 
-print("parent_ids_m1:", parent_ids_m1)
-print("parent_ids_m2:", parent_ids_m2)
-print(len(shower_particles))
-print(len(parent_ids_m1))
-print(len(parent_ids_m2))
-print(particle_x)
-print(particle_y)
-print(particle_z)
-
 fig, ax_polar = plt.subplots(1, 1, subplot_kw=dict(projection='polar'), figsize=(10, 8))
 for i in range(len(shower_particles)):
     ax_polar.scatter(particle_phi[i], particle_eta[i], c=particle_en[i], cmap='viridis', s=50, alpha=0.7)
     if parent_ids_m1[i] != 0:
-
         ax_polar.plot([event[parent_ids_m1[i]].phi(), particle_phi[i]],
-                [event[parent_ids_m1[i]].eta(), particle_eta[i]], color='gray', linestyle='--')
+                      [event[parent_ids_m1[i]].eta(), particle_eta[i]], color='gray', linestyle='--')
     if parent_ids_m2[i] != 0:
         ax_polar.plot([event[parent_ids_m2[i]].phi(), particle_phi[i]],
-                [event[parent_ids_m2[i]].eta(), particle_eta[i]], color='gray', linestyle='--')
+                      [event[parent_ids_m2[i]].eta(), particle_eta[i]], color='gray', linestyle='--')
 
 ax_polar.set_title('Polar Coordinates')
 cbar_polar = plt.colorbar(ax_polar.scatter([], [], c=[], cmap='viridis', s=50, alpha=0.7), ax=ax_polar, orientation='vertical')
@@ -119,19 +119,19 @@ for i in range(len(shower_particles)):
         ax_3d.plot([particle_x[parent_ids_m2[i]], particle_x[i]],
                    [particle_y[parent_ids_m2[i]], particle_y[i]],
                    [particle_z[parent_ids_m2[i]], particle_z[i]], color='gray', linestyle='--')
+
 ax_3d.set_xlabel('X')
 ax_3d.set_ylabel('Y')
 ax_3d.set_zlabel('Z')
 ax_3d.set_title('3D Coordinates')
 
+ax_3d.set_xlim(-25, 25)
+ax_3d.set_ylim(-25, 25)
+ax_3d.set_zlim(-25, 25)
 
-ax_3d.set_xlim(-0.0000000000008, 0.0000000000008)
-ax_3d.set_ylim(-0, 0.0000000000008)
-ax_3d.set_zlim(-0.01, 0.01)
 
 cbar_ax_3d = plt.colorbar(sc, ax=ax_3d, orientation='vertical')
 cbar_ax_3d.set_label('Energy')
 plt.show()
-
 num_points = len(sc.get_offsets())
 print(f"Number of points: {num_points}")
